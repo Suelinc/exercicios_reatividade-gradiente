0f02dea00bd2fc95e75bf050c320d2587921cde3
Suelinc <104871153+Suelinc@users.noreply.github.com>
2022-06-13 20:02:57 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/exercicios_reatividade/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.exercicios_reatividade
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Gradient'),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Colors.purple, Colors.blueAccent])),
+        child: const Center(
+          child: Text(
+            'Flutter Gradient!',
+            style: TextStyle(
+              fontSize: 48.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
